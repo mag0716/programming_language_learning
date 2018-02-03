a9d2ff8e6cd20f994de1661c9d578b9b89c058ae
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-02-03 21:51:08 +0900

    Q11の回答追加

--- Kotlin/Sample/src/Answer11.kt
+++ Kotlin/Sample/src/Answer11.kt
@@ -1,34 +1,42 @@
-fun main(args: Array<String>) {
-    println(isDivided(2))
-    println(isDivided(3))
-    println(isDivided(5))
-    println(isDivided(8))
-    println(isDivided(21))
-    println(isDivided(144))
 
-    val fibonacciNumbers = mutableListOf<Int>(1, 1)
-    generateFibonacci(fibonacciNumbers, 10)
-    println(fibonacciNumbers)
+fun main(args: Array<String>) {
+    //println(isDivided(2))
+    //println(isDivided(3))
+    //println(isDivided(5))
+    //println(isDivided(8))
+    //println(isDivided(21))
+    //println(isDivided(144))
+
+    val fibonacciNumbers = mutableListOf(1L, 1L)
+    val dividableFibonacciNumbers = mutableListOf<Long>()
+    generateFibonacci(fibonacciNumbers, dividableFibonacciNumbers) {
+        // 2, 3, 5, 8, 21, 144 に続く 5つを発見したら終了
+        it.size <= 10
+    }
+    println(dividableFibonacciNumbers)
 }
 
 /**
  * 指定したcount分までフィボナッチ数列を生成する
  */
-fun generateFibonacci(fibonacci: MutableList<Int>, count: Int) {
-    if(count > 0) {
+fun generateFibonacci(fibonacci: MutableList<Long>, dividableFibonacci: MutableList<Long>, isContinue:(MutableList<Long>)->Boolean) {
+    if(isContinue(dividableFibonacci)) {
         val nextNumber = fibonacci.takeLast(2).sum()
         fibonacci.add(nextNumber)
-        generateFibonacci(fibonacci, count-1)
+        if(isDivided(nextNumber)) {
+            dividableFibonacci.add(nextNumber)
+        }
+        generateFibonacci(fibonacci, dividableFibonacci, isContinue)
     }
 }
 
 /**
  * 各桁の数字を足した数で自身が割り切れるかどうか
  */
-fun isDivided(number: Int) : Boolean {
+fun isDivided(number: Long) : Boolean {
     val numberStr = number.toString()
     if(numberStr.length >= 2) {
-        return number % generateSumOfAllDigits(numberStr) == 0
+        return number % generateSumOfAllDigits(numberStr) == 0L
     }
     return true
 }

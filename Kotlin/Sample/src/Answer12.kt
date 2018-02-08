e4051deaee5662f367d0b61c41b467e5f9a6ec34
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-02-08 22:45:07 +0900

    Q12の回答追加
    ・コンマを除く10文字内に0-9が全て存在するような値があるかをチェックする

--- Kotlin/Sample/src/Answer12.kt
+++ Kotlin/Sample/src/Answer12.kt
@@ -1,26 +1,41 @@
 import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
-    println(searchSqrtThatExistsAllDigits())
-    println(searchSqrtThatExistsAllDigits(true))
+    println("include integer : ${searchSqrtThatExistsAllDigits()}")
+    println("only decimal : ${searchSqrtThatExistsAllDigits(true)}")
 }
 
 fun searchSqrtThatExistsAllDigits(onlyDecimal : Boolean = false) : Int {
     var value = 1
     while(true) {
-        if(existsAllDigits(sqrtString(value, onlyDecimal))) {
+        val sqrtString = sqrtString(value)
+        val targetString = removeCommaAndMinimumLength(sqrtString, onlyDecimal)
+        if(existsAllDigits(targetString)) {
             return value
         }
         value++
     }
 }
 
-fun sqrtString(value: Int, onlyDecimal : Boolean = false) : String {
+fun sqrtString(value: Int) : String {
     if(value <= 0) {
         throw IllegalArgumentException("must be value > 0")
     }
-    val sqrtString = Math.sqrt(value.toDouble()).toString()
-    return if(onlyDecimal) sqrtString.substring(2) else sqrtString
+    return Math.sqrt(value.toDouble()).toString()
+}
+
+/**
+ * コンマを取り除き、最小文字数(10)に切り出す
+ */
+fun removeCommaAndMinimumLength(string: String, onlyDecimal: Boolean = false) : String {
+    val removedCommaString = if(onlyDecimal) {
+        val commaIndex = string.indexOf(".")
+        string.substring(commaIndex+1)
+    } else {
+        string.replace(".", "")
+    }
+
+    return if(removedCommaString.length < 10) removedCommaString else removedCommaString.substring(0..9)
 }
 
 fun existsAllDigits(str: String) : Boolean {

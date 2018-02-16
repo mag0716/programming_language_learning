5bfa3e3f06cf84747ce0495fec0fe1156ef54458
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-02-16 22:21:55 +0900

    Q13の回答(途中)
    ・覆面算を計算するためのクラスを追加

---
+++ Kotlin/Sample/src/Answer13.kt
@@ -0,0 +1,66 @@
+fun main(args: Array<String>) {
+    val validCalculation = MaskCalculation(1234567890)
+    println("${validCalculation.valid()} : ${validCalculation.leftSide()} = ${validCalculation.rightSide()}")
+
+    val invalidCalculation = MaskCalculation(1111111111)
+    println("${invalidCalculation.valid()} : ${invalidCalculation.leftSide()} = ${invalidCalculation.rightSide()}")
+}
+
+/**
+ * 覆面算
+ *
+ * READ + WRITE + TALK = SKILL
+ * 最上位の文字には 0 はありえない
+ */
+class MaskCalculation(val value: Long) {
+    val R: Int
+    val W: Int
+    val T: Int
+    val S: Int
+    val E: Int
+    val A: Int
+    val D: Int
+    val I: Int
+    val L: Int
+    val K: Int
+
+    init {
+        if(value < 1000000000) {
+            throw IllegalArgumentException("invalid argument : $value")
+        }
+
+        val str = value.toString()
+        R = str[0].toString().toInt()
+        W = str[1].toString().toInt()
+        T = str[2].toString().toInt()
+        S = str[3].toString().toInt()
+        E = str[4].toString().toInt()
+        A = str[5].toString().toInt()
+        D = str[6].toString().toInt()
+        I = str[7].toString().toInt()
+        L = str[8].toString().toInt()
+        K = str[9].toString().toInt()
+    }
+
+    fun valid() : Boolean {
+        if(R == 0 || W == 0 || T == 0 || S == 0) {
+            return false
+        }
+        return isAllDifferentDigit()
+    }
+
+    fun leftSide() : Int {
+        return "$R$E$A$D".toInt() + "$W$R$I$T$E".toInt() + "$T$A$L$K".toInt()
+    }
+
+    fun rightSide() : Int {
+        return "$S$K$I$L$L".toInt()
+    }
+
+    private fun isAllDifferentDigit() : Boolean {
+        return value.toString()
+                .toCharArray()
+                .distinct()
+                .count() == 10
+    }
+}

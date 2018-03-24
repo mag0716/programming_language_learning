2b5c8d967e4da6d00be9b167184589c1d77cd68f
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-03-24 22:37:59 +0900

    ヒントを利用して Q17 の回答追加

--- Kotlin/Sample/src/Answer17.kt
+++ Kotlin/Sample/src/Answer17.kt
@@ -2,18 +2,47 @@ fun main(args: Array<String>) {
     println("Hello Answer17")
 
     // expect BBBB
-    println(listOf("B", "B", "B", "B").permutation())
+    // println(listOf("B", "B", "B", "B").permutation())
 
     // expect GBBB, BGBB, BBGB, BBBG
-    println(listOf("G", "B", "B", "B").permutation())
+    // println(listOf("G", "B", "B", "B").permutation())
 
     // expect : 8
-    val allPattern = calculatePattern(4)
-    println(allPattern)
-    println(filterContinuousGirl(allPattern))
+    // val allPattern = calculatePattern(4)
+    // println(allPattern)
+    // println(filterContinuousGirl(allPattern))
 
     // Answer
-    println(filterContinuousGirl(calculatePattern(30)).size)
+    //println(filterContinuousGirl(calculatePattern(30)).size)
+
+    // expect : 8
+    println(calculatePattern(mutableListOf(), 4).size)
+
+    // Answer
+    println(calculatePattern(mutableListOf(), 30).size)
+}
+
+fun calculatePattern(allPattern: MutableList<String>, numberOfPeople: Int) : List<String> {
+    val tmp = mutableListOf<String>()
+    if(allPattern.isNotEmpty()) {
+        for (pattern in allPattern) {
+            val lastPerson: Char? = if (pattern.isNotEmpty()) pattern[pattern.lastIndex] else null
+            if (lastPerson != null && lastPerson.equals('G')) {
+                tmp.add("${pattern}B")
+            } else {
+                tmp.add("${pattern}B")
+                tmp.add("${pattern}G")
+            }
+        }
+    } else {
+        tmp.add("B")
+        tmp.add("G")
+    }
+    if(0 < numberOfPeople) {
+        return calculatePattern(tmp, numberOfPeople-1)
+    } else {
+        return allPattern
+    }
 }
 
 fun calculatePattern(numberOfPeople : Int) : List<List<String>> {

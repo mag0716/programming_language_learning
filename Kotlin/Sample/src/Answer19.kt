2945eeb4087b2a346a6b73dcf53fa48c043b3c07
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-04-21 21:58:53 +0900

    Q19の回答追加(NG)

--- Kotlin/Sample/src/Answer19.kt
+++ Kotlin/Sample/src/Answer19.kt
@@ -11,6 +11,15 @@ fun main(args: Array<String>) {
 
     // expect : [4, 6, 8, 9, 10]
     println(calculateCompositeNumbers(10))
+
+    // expect : [2, 3, 5, 7, 11, 13]
+    val primeNumbers = calculatePrimeNumbers(6)
+    println(primeNumbers)
+
+    // answer
+    // 素数を小さい順番から掛ければ条件を満たす合成数が求められる
+    // [ 4, 6, 15, 35, 77, 143, 169]
+    println(primeNumbers[primeNumbers.lastIndex] * primeNumbers[primeNumbers.lastIndex])
 }
 
 fun Int.isPrime() : Boolean {
@@ -35,4 +44,16 @@ fun calculateCompositeNumbers(n : Int) : List<Int> {
     }
 
     return compositeNumbers
+}
+
+fun calculatePrimeNumbers(n : Int) : List<Int> {
+    val primeNumbers = mutableListOf<Int>()
+    var number = 2
+    while(primeNumbers.size < 6) {
+        if(number.isPrime()) {
+            primeNumbers.add(number)
+        }
+        number++
+    }
+    return primeNumbers
 }

c84149b386cb101b9a63a99b64d62d1bf142c654
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-04-27 23:25:26 +0900

    combination を求めるメソッドを追加

--- Kotlin/Sample/src/Permutation.kt
+++ Kotlin/Sample/src/Permutation.kt
@@ -11,3 +11,19 @@ fun <T> List<T>.permutation():List<List<T>>{
         }
     return perms.distinct()
 }
+
+fun <T> Collection<T>.powerset(): Set<Set<T>> = powerset(this, setOf(setOf()))
+
+private tailrec fun <T> powerset(left: Collection<T>, acc: Set<Set<T>>): Set<Set<T>> = when {
+    left.isEmpty() -> acc
+    else ->powerset(left.drop(1), acc + acc.map { it + left.first() })
+}
+
+fun <T> Set<T>.combinations(combinationSize: Int): Set<Set<T>> = when {
+    combinationSize < 0 -> throw IllegalArgumentException("invalid combinationSize : $combinationSize")
+    combinationSize == 0 -> setOf(setOf())
+    combinationSize >= size -> setOf(toSet())
+    else -> powerset()
+            .filter { it.size == combinationSize }
+            .toSet()
+}
